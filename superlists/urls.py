67c924c2d8ef14b9899cff3dd99d1512a1624f66
Frank Berghaus <frank.berghaus@cern.ch>
2016-01-13 20:07:02 +0100

    Add URL, view and template to display lists
    Progressing towards allowing unique lists for different users. For now
    only have 1 list, but starting to change how we fill and display it.

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from lists import views
+from lists import views as lists_views
 
 urlpatterns = [
     # Examples:
-    url(r'^$', views.home_page, name='home'),
+    url(r'^$', lists_views.home_page, name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', lists_views.view_list, name='view_list'),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
