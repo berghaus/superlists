439c5ffddbb5a820dff64ef090ab9418396f921f
Frank Berghaus <frank.berghaus@cern.ch>
2016-04-03 13:18:38 +0200

    added login link which does nothing

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -14,7 +14,7 @@ class LoginTest(FunctionalTest):
                     return
             retries -= 1
             time.sleep(0.5)
-        self.fail('could not find window: ',text_in_title)
+        self.fail('could not find window: '+text_in_title)
 
     def wait_for_element_with_id(self,element_id):
         WebDriverWait(self.browser, timeout=30).until(
@@ -25,7 +25,7 @@ class LoginTest(FunctionalTest):
         # Edith goes to the super-duper lists website
         # and notices the "Sign In" link for the first time.
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('login').click()
+        self.browser.find_element_by_id('id_login').click()
 
         # A persona login box appears
         self.switch_to_new_window('Mozilla Persona')
@@ -40,6 +40,6 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # She can see that she is logged in
-        self.wait_for_element_with_id('logout')
+        self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('edith@mockmyid.com', navbar.text)
