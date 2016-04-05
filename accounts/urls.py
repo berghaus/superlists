50b6b59004a30d172a57f8b7c0f5e5e7ee55fb24
Frank Berghaus <frank.berghaus@cern.ch>
2016-04-05 19:50:28 +0200

    test session is persistent. implement logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+#from django.contrib.auth.views import logout as django_logout
 from accounts import views as accounts_views
 
 urlpatterns = [
     url(r'^login$', accounts_views.persona_login, name='persona_login'),
+    url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout' )
 ]
