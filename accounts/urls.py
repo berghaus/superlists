7876888ba4ec53e15ff98024471953ca33f5eb61
Frank Berghaus <frank.berghaus@cern.ch>
2016-04-08 16:18:42 +0200

    hand callable to urls to be ready for django 1.10

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 #from django.contrib.auth.views import logout as django_logout
 from accounts import views as accounts_views
+from django.contrib.auth.views import logout
 
 urlpatterns = [
     url(r'^login$', accounts_views.persona_login, name='persona_login'),
-    url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout' )
+    url(r'^logout$', logout, {'next_page': '/'}, name='logout' )
 ]
