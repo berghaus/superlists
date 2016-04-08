8549a149ea4a77096906bc01bfaa96ce8bf6f98d
Frank Berghaus <frank.berghaus@cern.ch>
2016-04-08 11:56:32 +0200

    add test_my_lists placeholder
    Aim to associate lists created by user with account. They would be made
    available as "My Lists" to the user. Start by writing a test! Make the login
    and logout checkers from test_login available for our new tests.

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,8 +1,7 @@
 from .base import FunctionalTest
-from selenium.webdriver.support.ui import WebDriverWait
 import time
 
-
+TEST_EMAIL = 'edith@mockmyid.com'
 class LoginTest(FunctionalTest):
 
     def switch_to_new_window(self, text_in_title):
@@ -16,24 +15,6 @@ class LoginTest(FunctionalTest):
             time.sleep(0.5)
         self.fail('could not find window: '+text_in_title)
 
-    def wait_for_element_with_id(self,element_id):
-        WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id),
-            'Could not find element with id {}. Page text was {}'.format(
-                element_id, self.browser.find_element_by_tag_name('body').text
-            )
-        )
-
-    def wait_to_be_logged_in(self):
-        self.wait_for_element_with_id('id_logout')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('edith@mockmyid.com', navbar.text)
-
-    def wait_to_be_logged_out(self):
-        self.wait_for_element_with_id('id_login')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn('edith@mockmyid.com', navbar.text)
-
     def test_login_with_persona(self):
         # Edith goes to the super-duper lists website
         # and notices the "Sign In" link for the first time.
@@ -46,24 +27,24 @@ class LoginTest(FunctionalTest):
         # Edith logs in with her email
         self.browser.find_element_by_id(
             'authentication_email'
-        ).send_keys('edith@mockmyid.com')
+        ).send_keys(TEST_EMAIL)
         self.browser.find_element_by_tag_name('button').click()
 
         # The Persona window closes
         self.switch_to_new_window('To-Do')
 
         # She can see that she is logged in
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(TEST_EMAIL)
 
         # Refreshing the page she sees it's a real session login, not just a
         # one-off for that page
         self.browser.refresh()
-        self.wait_to_be_logged_in()
+        self.wait_to_be_logged_in(TEST_EMAIL)
 
         # terrified of the new feature she quickly hits "logout"
         self.browser.find_element_by_id('id_logout').click()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(TEST_EMAIL)
 
         # the "logged out" status also persists after a refresh
         self.browser.refresh()
-        self.wait_to_be_logged_out()
+        self.wait_to_be_logged_out(TEST_EMAIL)
